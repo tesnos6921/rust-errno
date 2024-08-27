e164a00611f6fd766888b61fa63248a693f12aac
tesnos6921 <7860497+tesnos6921@users.noreply.github.com>
2024-08-27 16:18:57 -0400

    Update unix.rs

--- src/unix.rs
+++ src/unix.rs
@@ -13,7 +13,7 @@
 // except according to those terms.
 
 use core::str;
-use libc::{self, c_int, size_t, strerror_r, strlen};
+use libc::{self, c_int, size_t, strerror, strlen};
 
 use crate::Errno;
 
@@ -30,16 +30,14 @@ where
 {
     let mut buf = [0u8; 1024];
     let c_str = unsafe {
-        let rc = strerror_r(err.0, buf.as_mut_ptr() as *mut _, buf.len() as size_t);
+        let rc = strerror(err.0, buf.as_mut_ptr() as *mut _, buf.len() as size_t);
         if rc != 0 {
             // Handle negative return codes for compatibility with glibc < 2.13
             let fm_err = match rc < 0 {
                 true => errno(),
                 false => Errno(rc),
             };
-            if fm_err != Errno(libc::ERANGE) {
-                return callback(Err(fm_err));
-            }
+            return callback(Err(fm_err));
         }
         let c_str_len = strlen(buf.as_ptr() as *const _);
         &buf[..c_str_len]
@@ -47,7 +45,7 @@ where
     callback(Ok(from_utf8_lossy(c_str)))
 }
 
-pub const STRERROR_NAME: &str = "strerror_r";
+pub const STRERROR_NAME: &str = "strerror";
 
 pub fn errno() -> Errno {
     unsafe { Errno(*errno_location()) }
